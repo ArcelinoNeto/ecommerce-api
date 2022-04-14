8a8b1d0183ab1a166eaab2135960cb69fd1eaaeb
ArcelinoNeto <arcelino.neto@gmail.com>
2022-04-14 17:03:37 -0300

    Inclusão do Model ProductCategor, suas validações e seus testes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_14_183222) do
+ActiveRecord::Schema.define(version: 2022_04_14_190526) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 2022_04_14_183222) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["system_requirement_id"], name: "index_games_on_system_requirement_id"
+  end
+
+  create_table "product_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_product_categories_on_category_id"
+    t.index ["product_id"], name: "index_product_categories_on_product_id"
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -75,4 +84,6 @@ ActiveRecord::Schema.define(version: 2022_04_14_183222) do
   end
 
   add_foreign_key "games", "system_requirements"
+  add_foreign_key "product_categories", "categories"
+  add_foreign_key "product_categories", "products"
 end
